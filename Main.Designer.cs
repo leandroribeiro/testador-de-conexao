bd92055e9a67fa0a1a546605c6320e415d601353
Leandro Ribeiro <github@leandroribeiro.com>
2020-05-30 18:07:40 -0300

    Código da primeira versão

---
+++ Main.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace TesteDeConexao
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBancoDeDados = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnTestar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txtPorta = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstHistorico = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome ou IP do Banco de Dados";
+            // 
+            // txtBancoDeDados
+            // 
+            this.txtBancoDeDados.Font = new System.Drawing.Font("Cascadia Code", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBancoDeDados.Location = new System.Drawing.Point(56, 67);
+            this.txtBancoDeDados.Name = "txtBancoDeDados";
+            this.txtBancoDeDados.Size = new System.Drawing.Size(314, 30);
+            this.txtBancoDeDados.TabIndex = 1;
+            this.txtBancoDeDados.Validating += new System.ComponentModel.CancelEventHandler(this.txtBancoDeDados_Validating);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Cascadia Code", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(111, 194);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(175, 30);
+            this.txtUsuario.TabIndex = 3;
+            this.txtUsuario.Validating += new System.ComponentModel.CancelEventHandler(this.txtUsuario_Validating);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Code", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(157, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Usuário";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Cascadia Code", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(111, 252);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(175, 30);
+            this.txtSenha.TabIndex = 5;
+            this.txtSenha.Validating += new System.ComponentModel.CancelEventHandler(this.txtSenha_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Code", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(168, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Senha";
+            // 
+            // btnTestar
+            // 
+            this.btnTestar.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnTestar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTestar.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTestar.ForeColor = System.Drawing.Color.White;
+            this.btnTestar.Location = new System.Drawing.Point(93, 312);
+            this.btnTestar.Name = "btnTestar";
+            this.btnTestar.Size = new System.Drawing.Size(109, 34);
+            this.btnTestar.TabIndex = 6;
+            this.btnTestar.Text = "TESTAR";
+            this.btnTestar.UseVisualStyleBackColor = false;
+            this.btnTestar.Click += new System.EventHandler(this.btnTestar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.Color.DarkRed;
+            this.btnFechar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFechar.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.ForeColor = System.Drawing.Color.White;
+            this.btnFechar.Location = new System.Drawing.Point(208, 312);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(109, 34);
+            this.btnFechar.TabIndex = 7;
+            this.btnFechar.Text = "FECHAR";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // txtPorta
+            // 
+            this.txtPorta.Font = new System.Drawing.Font("Cascadia Code", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPorta.Location = new System.Drawing.Point(111, 136);
+            this.txtPorta.Name = "txtPorta";
+            this.txtPorta.Size = new System.Drawing.Size(175, 30);
+            this.txtPorta.TabIndex = 9;
+            this.txtPorta.Text = "5433";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cascadia Code", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(168, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Porta";
+            // 
+            // lstHistorico
+            // 
+            this.lstHistorico.Font = new System.Drawing.Font("Cascadia Code", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstHistorico.FormattingEnabled = true;
+            this.lstHistorico.HorizontalScrollbar = true;
+            this.lstHistorico.ItemHeight = 16;
+            this.lstHistorico.Location = new System.Drawing.Point(12, 368);
+            this.lstHistorico.Name = "lstHistorico";
+            this.lstHistorico.Size = new System.Drawing.Size(399, 132);
+            this.lstHistorico.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(302, 506);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 34);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "COPIAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 549);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstHistorico);
+            this.Controls.Add(this.txtPorta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnTestar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBancoDeDados);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Testador de Conexão by Leandro Ribeiro";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBancoDeDados;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnTestar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TextBox txtPorta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstHistorico;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TesteDeConexao
+{
+    public partial class Main : Form
+    {
+        private const string BancoDeDados = "master";
+        private string _cnxStr;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestar_Click(object sender, EventArgs e)
+        {
+            if (!ValidateChildren(ValidationConstraints.Enabled))
+                return;
+
+            AdicionarMensagemNoHistorico("Testando...");
+
+            BloquearBotao();
+
+            var servidor = txtBancoDeDados.Text;
+            var porta = txtPorta.Text;
+            var bancoDeDados = BancoDeDados;
+            var usuario = txtUsuario.Text;
+            var senha = txtSenha.Text;
+
+            var dt = new DataTable();
+            _cnxStr = $"Data Source={servidor},{porta};Initial Catalog={bancoDeDados};Persist Security Info=True;User Id={usuario};Password={senha};MultipleActiveResultSets=True;App=TesteDeConexao";
+
+            using (var conn = new SqlConnection(_cnxStr))
+            {
+
+                try
+                {
+                    var sqlComm = new SqlCommand("SELECT @@VERSION", conn)
+                    {
+                        CommandType = CommandType.Text
+                    };
+
+                    var da = new SqlDataAdapter { SelectCommand = sqlComm };
+
+                    da.Fill(dt);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        AdicionarMensagemNoHistorico("Conectado :)");
+                        AdicionarMensagemNoHistorico($"CnxStr {_cnxStr}");
+                    }
+
+                    var versao = dt.Rows[0][0].ToString();
+
+                    AdicionarMensagemNoHistorico($"Versão {versao}");
+                }
+                catch (Exception ex)
+                {
+                    AdicionarMensagemNoHistorico("Falhou :\\");
+                    AdicionarMensagemNoHistorico(ex.Message);
+                }
+
+            }
+
+            HabilitarBotao();
+        }
+
+        private void HabilitarBotao()
+        {
+            btnTestar.Text = "TESTAR";
+            btnTestar.Enabled = true;
+            btnTestar.Cursor = Cursors.Hand;
+        }
+
+        private void BloquearBotao()
+        {
+            btnTestar.Text = "AGUARDE...";
+            btnTestar.Enabled = false;
+            btnTestar.Cursor = Cursors.No;
+        }
+
+        private void AdicionarMensagemNoHistorico(string mensagem)
+        {
+            var texto = $"{DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")} - {mensagem}";
+
+            lstHistorico.Items.Insert(0, texto);
+            lstHistorico.SelectedIndex = 0;
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtBancoDeDados_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBancoDeDados.Text))
+            {
+                e.Cancel = true;
+                txtBancoDeDados.Focus();
+                errorProvider1.SetError(txtBancoDeDados, "Você precisa preencher o nome ou IP do banco de dados");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(txtBancoDeDados, "");
+            }
+        }
+
+        private void txtUsuario_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtUsuario.Text))
+            {
+                e.Cancel = true;
+                txtUsuario.Focus();
+                errorProvider1.SetError(txtUsuario, "Você precisa preencher o nome do usuário");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(txtUsuario, "");
+            }
+        }
+
+        private void txtSenha_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtSenha.Text))
+            {
+                e.Cancel = true;
+                txtSenha.Focus();
+                errorProvider1.SetError(txtSenha, "Você precisa preencher a senha");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(txtSenha, "");
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+#if DEBUG
+            txtBancoDeDados.Text = "localhost";
+            txtUsuario.Text = "sa";
+            txtSenha.Text = "Pass@word";
+#endif
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var text = lstHistorico.Items.Cast<object>().Aggregate("", (current, item) => current + (item.ToString() + "\r\n"));
+
+            Clipboard.SetText(text);
+
+            MessageBox.Show("Copiado para área de transferência, utilize Ctr + V para colar...");
+        }
+    }
+}
